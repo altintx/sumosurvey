14efc7379e5daea66a03512c1e74de8ada933c7f
Brian Bosh <bbosh@Fortress.local>
2016-04-20 18:00:53 -0600

    retrieve q+a

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
 var express = require("express");
-var Sequelize = require('sequelize');
-
-var conn = {};
-if (!("MYSQLHOST" in process.env && "MYSQLUSER" in process.env && "MYSQLPASSWORD" in process.env)) {
-	console.error("Expecting: MYSQLHOST=... MYSQLUSER=... MYSQLPASSWORD=... " + process.argv.join(" "));
-	process.exit();
-} else {
-	conn.host = process.env.MYSQLHOST;
-	conn.user = process.env.MYSQLUSER;
-	conn.pass = process.env.MYSQLPASS;
-}
-var db = new Sequelize(conn.host, conn.user, conn.pass);
-
+var db = require('./models');
+var Promise = require("rsvp").Promise;
 
 var server = express();
 
-var Question = db.define('survey_question', {
-  question: Sequelize.STRING
+express.static("public");
+
+
+// db.Question, db.Answer, db.Response
+server.get("/api/questions", function(req, res) {
+	db.Question.findAll().then(function(questions) {
+		res.send(questions);
+	})
 });
-var Answer = db.define('survey_question', {
-  question: Sequelize.STRING
+
+server.get("/api/questions/:id", function(req, res) {
+	db.Question.findById(req.params.id).then(function(question) {
+		if (question)
+			db.Answer.findAll({ questionId: question.id }).then(function(answers) {
+				res.send({
+					success: true,
+					question: question.get().text,
+					answers: answers.map(function(a) {
+						return a.get();
+					})
+				});
+			});
+		else {
+			res.send({ success: false, message: "Unknown question" });
+		}
+	});
 });
 
+server.post("/api/questions", function(req, res) {
+	if (!("question" in req.params && "answers" in req.params && typeof params.answers == "Object")) {
+		return res.send({
+			success: false,
+			message: "Send " + JSON.stringify({ "question": "Question Text", "Answers": [] })
+		});
 
-server.get("/", function(req, res) {
-
-})
+	};
+	var questionId = 0;
+	db.Question.create({
+		text: req.params.question
+	}).then(function(q) {
+		questionId = q.id;
+		return Promise.all(req.params.answers.map(function(answer) {
+			return db.Answer.create({
+				text: answer,
+				questionId: questionId
+			}).then(done);
+		}));
+	}).then(function(answers) {
+		res.redirect("/api/questions/" + questionId);
+	});
+});
 
 server.listen(3000);
